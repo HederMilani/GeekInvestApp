cc03d7d13a86e7f2233f4ad4bfe1d47f83e83762
Heder Milani <hedermilani@MacBook-Air-de-Heder.local>
2022-09-07 19:58:02 -0300

    ADD - Adicionar Instituição ViewSet

--- GeekInvest/Instituicao/views.py
+++ GeekInvest/Instituicao/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# <---------------Importar Bibliotecas---------------------->
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+# <---------------Importar Componentes--------------------->
+from .models import InstituicaoModel
+from .serializers import InstituicaoSerializers
+
+# <-----------------Classe Visualização dos dados pela API----------------------->
+class InstituicaoViewSet(viewsets.ModelViewSet):
+    queryset = InstituicaoModel.objects.all()
+    serializer_class = InstituicaoSerializers
+    #permission_classes = [permissions.IsAuthenticated]
